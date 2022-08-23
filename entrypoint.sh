c5c147e76d27f6f7eb3405e679da8dff83debeb3
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-08-23 19:43:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-python3 collect_metrics.py "$1" "$2"
+metrics_path = "/github/workspace/$1"
+
+echo "cwd contents"
+ls -lha .
+
+echo "root contents"
+ls -lha /
+
+python /collect_metrics.py "$metrics_path" "$2"
