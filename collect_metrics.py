4ea88670ec5725c61d3a691a05853af5d469aab5
Grahame White <4whitg16@solent.ac.uk>
2022-08-23 15:42:46 +0100

    lint

--- collect_metrics.py
+++ collect_metrics.py
@@ -2,18 +2,17 @@ import json
 import os
 import sys
 
-import pymongo as pymongo
-
 
 def main(args):
     """
     Collect some metrics and save to a json file
     """
     metrics_data = {
-    "name": "some test repo",
-    "data": "some other data that was collected"
+        "name": "some test repo",
+        "data": "some other data that was collected"
     }
     save_doc(args[1], args[2], metrics_data)
+
 
 def save_doc(metrics_path, collection, data):
     if os.path.exists(metrics_path):
