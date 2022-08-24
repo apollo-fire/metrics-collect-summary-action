f1426485cdc34f1fd3b66c9be25d5521df678ce7
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-08-24 17:19:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@
 # 2 - name of collection to save to
 # 3 - path of cloned repo, e.g. /github/workspace/
 
+git config --global -add safe.directory "/github/workspace"
+
 # Where to save metrics file to
 metrics_path="/github/workspace/$1"
 python3 /collect_metrics.py "$metrics_path" "$2" "/github/workspace"
