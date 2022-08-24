43a24844dd4da0afaa4c9ccb5e889fcd64bfc20c
Grahame White <4whitg16@solent.ac.uk>
2022-08-24 16:38:58 +0100

    lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-metrics_path="/github/workspace/$1"
+# 1 - name of metrics directory
+# 2 - name of collection to save to
+# 3 - path of cloned repo, e.g. /github/workspace/
 
-python3 /collect_metrics.py "$metrics_path" "$2"
+# Where to save metrics file to
+metrics_path="/github/workspace/$1"
+python3 /collect_metrics.py "$metrics_path" "$2" "$3"
